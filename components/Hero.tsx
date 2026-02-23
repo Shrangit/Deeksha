05506a7a1e86463d1dd414ae2ac16f6285ad43d4
BotnBolt <shrangit.botnbolt@gmail.com>
2026-02-24 01:17:00 +0530

    Fix background image path

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
             {/* Background Image */}
             <div className="absolute inset-0 z-0">
                 <Image
-                    src="/df.png"
+                    src="/banner.png"
                     alt="Timeless Modest Wear Background"
                     fill
                     priority
